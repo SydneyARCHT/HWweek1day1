a102ee0cf083795325dbd337100ea644cea6d0cb
SydneyARCHT <sstiward@hotmail.com>
2024-06-10 12:50:27 -0700

    First Homework Attempt / First Commit

---
+++ homework.py
@@ -0,0 +1,25 @@
+# Task 1:
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2:
+variable_a = "Hello, World!"
+variable_b = 23
+variable_c = 3.14
+variable_d = True
+
+print(type(variable_a)) #str
+print(type(variable_b)) #int
+print(type(variable_c)) #float
+print(type(variable_d)) #bool
+
+#Task 3:
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+grocery_total = bread + peanut_butter + jelly
+print(grocery_total) # $10.48 is the total
